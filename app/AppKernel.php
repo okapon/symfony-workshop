434f4a53bd2c462093e7026d539d3cfc75e1b39b
Yuichi Okada <yuuichi177@gmail.com>
2013-05-17 02:20:12 +0900

    MyBlogBundleを生成

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new My\BlogBundle\MyBlogBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
